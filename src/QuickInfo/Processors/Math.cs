aae51d7f351669208a45ace08f209d4bbfeb5765
Kirill Osenkov <github@osenkov.com>
2017-09-02 22:45:04 -0700

    Add a random number generator ("rnd", "random", "random(N)", "random(LB, UB)").

--- src/QuickInfo/Processors/Math.cs
+++ src/QuickInfo/Processors/Math.cs
@@ -6,6 +6,43 @@ namespace QuickInfo
 {
     public class Math : IProcessor
     {
+        private static readonly Random random = new Random();
+
+        public static double Random(double lowerBound, double upperBound)
+        {
+            return lowerBound + Random() * (upperBound - lowerBound);
+        }
+
+        public static double Rnd(double lowerBound, double upperBound)
+        {
+            return Random(lowerBound, upperBound);
+        }
+
+        public static double Random(double upperBound)
+        {
+            return Random() * upperBound;
+        }
+
+        public static double Rnd(double upperBound)
+        {
+            return Random(upperBound);
+        }
+
+        public static double Random()
+        {
+            return random.NextDouble();
+        }
+
+        public static double Rnd()
+        {
+            return Random();
+        }
+
+        static Math()
+        {
+            Binder.Default.RegisterStaticMethods<Math>();
+        }
+
         public string GetResult(Query query)
         {
             var compiler = new Compiler();
