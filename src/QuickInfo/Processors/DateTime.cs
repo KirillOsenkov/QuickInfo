ffddbc9014267146fa3daf7b595a831e319168ab
Vladimir Rybalko <vladimir.a.rybalko@gmail.com>
2017-12-22 13:49:25 +0100

    Add UTC

---
+++ src/QuickInfo/Processors/DateTime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using static QuickInfo.HtmlFactory;
+
+namespace QuickInfo.Processors
+{
+    public class DateTime : IProcessor
+    {
+        private HashSet<string> triggerStrings = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            "utc"
+        };
+
+        public string GetResult(Query query)
+        {
+            if (query.IsHelp)
+            {
+                return HelpTable(("utc", "Current UTC date"));
+            }
+
+            if (triggerStrings.Contains(query.OriginalInput.Trim()))
+            {
+                return System.DateTime.UtcNow.ToString();
+            }
+
+            return null;
+        }
+    }
+}
