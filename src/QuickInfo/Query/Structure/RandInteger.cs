2ffe2fa7c97f5e2bfd144f8935ed208b91a6aebc
Vladimir Rybalko <vladimir.a.rybalko@gmail.com>
2017-12-22 15:28:03 +0100

    #2. Add random rgb.

---
+++ src/QuickInfo/Query/Structure/RandInteger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace QuickInfo
+{
+    public class RandInteger : IStructureParser
+    {
+        private readonly Regex _regex = new Regex(@"^(?:rnd\*(\d+)|(\d+)\*rnd)$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private readonly Random _rnd = new Random();
+                                             
+        public object TryParse(string query)
+        {
+            var trimmed = query.Trim();
+
+            var match = _regex.Match(trimmed);
+
+            if (match.Success)
+            {
+                var numberGroup = !string.IsNullOrEmpty(match.Groups[1].Value) ? match.Groups[1].Value : match.Groups[2].Value;
+                int number;
+                if (!int.TryParse(numberGroup, out number))
+                {
+                    return null;
+                }
+                else
+                {                    
+                    return number <= 255 ? new Integer(_rnd.Next(number + 1)) : null;
+                }
+            }
+
+            return null;
+        }
+    }
+}
