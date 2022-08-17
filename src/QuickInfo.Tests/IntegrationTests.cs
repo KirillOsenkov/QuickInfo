2cd91840e3afb603f9bb1321e4e4a47c6ada5a35
Cédric Luthi <cedric.luthi@gmail.com>
2022-08-17 14:49:33 +0200

    Add basic integration tests

---
+++ src/QuickInfo.Tests/IntegrationTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace QuickInfo.Tests
+{
+    public class IntegrationTests
+    {
+        [Theory]
+        [InlineData("a",         @"<div class=""mainAnswerText"">LATIN SMALL LETTER A</div>")]
+        [InlineData("%C3%A9",    @"<div class=""mainAnswerText"">LATIN SMALL LETTER E WITH ACUTE</div>")]
+        [InlineData("%E2%80%AF", @"<div class=""mainAnswerText"">NARROW NO-BREAK SPACE</div>")]
+        public async Task TestQuery(string query, string output)
+        {
+            await using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+
+            var response = await client.GetStringAsync($"api/Answers?query={query}");
+
+            Assert.Contains(output, response);
+        }
+    }
+}
