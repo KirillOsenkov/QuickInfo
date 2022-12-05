290f61d3c1583ffd8fd4d80c27b9dfc6a503b146
Kirill Osenkov <github@osenkov.com>
2022-12-04 19:36:00 -0800

    Add a test for currency

--- src/QuickInfo.Tests/IntegrationTests.cs
+++ src/QuickInfo.Tests/IntegrationTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
@@ -12,14 +12,28 @@ namespace QuickInfo.Tests
         [InlineData("%E2%80%AF", @"<div class=""mainAnswerText"">NARROW NO-BREAK SPACE</div>")]
         [InlineData("U%2BA7FB",  @"<div class=""mainAnswerText"">LATIN EPIGRAPHIC LETTER REVERSED F</div>")]
         [InlineData("F0 9F 8D 92 F0 9F 8D 87", @"🍒🍇")]
+        [InlineData("100 EUR", "100")]
         public async Task TestQuery(string query, string output)
+        {
+            var response = await GetResponse(query);
+            Assert.Contains(output, response);
+        }
+
+        [Theory]
+        [InlineData("100 EUR in USD", @"100")]
+        public async Task TestCurrency(string query, string output)
+        {
+            var response = await GetResponse(query);
+            Assert.Contains(output, response);
+        }
+
+        public async Task<string> GetResponse(string query)
         {
             await using var application = new WebApplicationFactory<Program>();
             using var client = application.CreateClient();
 
             var response = await client.GetStringAsync($"api/Answers?query={query}");
-
-            Assert.Contains(output, response);
+            return response;
         }
     }
 }
